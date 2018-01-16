ed2643089fc6ae2a4bf7f05d92b54bf75032e199
programmiertier <zimmermannveronique@protonmail.com>
2018-01-16 14:51:46 +0100

    FileStream BinaryWriter
    schreiben

--- BinärDatei/BinärDatei/Program.cs
+++ BinärDatei/BinärDatei/Program.cs
@@ -16,6 +16,31 @@ namespace BinärDatei
 
             FileStream stream = new FileStream(@"C:\Users\Zimmermann\Documents\datei.txt", FileMode.Create);
             BinaryWriter schreiben = new BinaryWriter(stream);
+
+            int zahl = 500;
+            schreiben.Write(zahl);
+            schreiben.Close();
+
+            // Datei öffnen und den Inhalt byteweise auslesen
+            FileInfo fi = new FileInfo(@"C:\Users\Zimmermann\Documents\datei.txt");
+            FileStream fs = new FileStream(@"C:\Users\Zimmermann\Documents\datei.txt", FileMode.Open);
+
+            byte[] byteArr = new byte[fi.Length];
+
+            // Datenstrom in ein Byte-Array lesen
+            fs.Read(byteArr, 0, (int)fi.Length);
+
+            // Ausgabe in der Konsole
+            WriteLine("Interpretation als Byte-Array:");
+            for (int zaehl = 0; zaehl < fi.Length; zaehl++)
+            {
+                WriteLine(byteArr[zaehl]);
+            }
+            WriteLine();
+            WriteLine();
+            fs.Close();
+
+            ReadLine();
         }
     }
 }
