0461a9c728fb05efe135263c97b21b2f00b403b4
programmiertier <zimmermannveronique@protonmail.com>
2018-01-16 09:28:03 +0100

    Text Label Eingaben
    mal sehen

--- DateiExplorer_WPF/DateiExplorer_WPF/MainWindow.xaml.cs
+++ DateiExplorer_WPF/DateiExplorer_WPF/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Console;
+using System.IO;
 
 namespace DateiExplorer_WPF
 {
@@ -23,6 +25,12 @@ namespace DateiExplorer_WPF
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void eingabe_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
         }
     }
 }
