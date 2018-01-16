574019ebf229c4a0d16f4bdb5cbd101cf4b0ae2c
programmiertier <zimmermannveronique@protonmail.com>
2018-01-16 13:50:03 +0100

    StreamWriter FileStream

---
+++ TextSchreiben/TextSchreiben/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace TextSchreiben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter schreib = new StreamWriter(@"C:\datei.txt");
+            FileStream stream = new FileStream(@"C:\datei.txt", FileMode.CreateNew);
+            StreamWriter schreib2 = new StreamWriter(stream);
+        }
+    }
+}
