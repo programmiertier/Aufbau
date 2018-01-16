5eb71185423dd6b0337d9ceb0021f19d0d2f8c8b
programmiertier <zimmermannveronique@protonmail.com>
2018-01-16 14:43:43 +0100

    BinärDatei mit Inhalt Int erstellen
    Binär in Datei

---
+++ BinärDatei/BinärDatei/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace BinärDatei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // eine Datei erzeugen und einen integer-Wert in die Datei schreiben
+
+            FileStream stream = new FileStream(@"C:\Users\Zimmermann\Documents\datei.txt", FileMode.Create);
+            BinaryWriter schreiben = new BinaryWriter(stream);
+        }
+    }
+}
