ee7ebb3ae88172ce605b2370e4fc60c02c583192
programmiertier <zimmermannveronique@protonmail.com>
2018-01-16 09:09:05 +0100

    Pfad
    Path und Anzeigen

--- Pfad/Pfad/Program.cs
+++ Pfad/Pfad/Program.cs
@@ -32,7 +32,33 @@ namespace Pfad
             // gibt Pfad zum Temp-Ordner zurück
             WriteLine(Path.GetTempPath());
 
-            Path.GetTempFileName();
+            // erzeugt eine Datei im Temp-Ordner und liefert den kompletten Pfad zurück
+            string tempDatei = Path.GetTempFileName();
+            // WriteLine(Path.GetTempFileName());
+            WriteLine(tempDatei);
+
+            // datei löschen
+            File.Delete(tempDatei);
+            if(File.Exists(tempDatei))
+            {
+                WriteLine("Datei ist noch da");
+            }
+            else
+            {
+                WriteLine("Temporäre Datei ist gelöscht worden");
+            }
+
+            DriveInfo laufwerk = new DriveInfo("c");
+            WriteLine(laufwerk.AvailableFreeSpace);
+            WriteLine(laufwerk.DriveFormat);
+            WriteLine(laufwerk.DriveType);
+            WriteLine(laufwerk.IsReady);
+            WriteLine(laufwerk.Name);
+            WriteLine(laufwerk.RootDirectory);
+            WriteLine(laufwerk.TotalFreeSpace);
+            WriteLine(laufwerk.TotalSize);
+            WriteLine(laufwerk.VolumeLabel);
+
             ReadLine();
         }
     }
