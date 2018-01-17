bf113696d35412bc8368b8032f373609aedd27f2
programmiertier <zimmermannveronique@protonmail.com>
2018-01-17 13:51:35 +0100

    Katzen anlegen lesen

---
+++ deKatzen/deKatzen/Katze.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DeKatzen
+{
+    internal class Katze
+    {
+        private int _katzenID;
+        private int _baujahr;
+        private double _futterStand;
+        private int _schlafLeistung;
+
+        public int katzenID { get { return _katzenID; } set { _katzenID = value; } }
+        public int baujahr { get { return _baujahr; } set { _baujahr = value; } }
+        public double futterStand { get { return _futterStand; } set { _futterStand = value; } }
+        public int schlafLeistung { get { return _schlafLeistung; } set { _schlafLeistung = value; } }
+
+        public Katze() { }
+
+        public Katze(int katzenID, int baujahr, double futterStand, int schlafLeistung )
+        {
+            WriteLine("Katze mit der ID {0}\tdem Jahr{1}\tFutterStand {2} und \tSchlafleistung {3}", 
+                        katzenID, baujahr, futterStand, schlafLeistung);
+        }
+    }
+}
---
+++ deKatzen/deKatzen/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace DeKatzen
+{
+    class Program : Katze
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Welches Katzerl hätten's denn gern?");
+            WriteLine(new string('=', 30));
+            ReadLine();
+            FileStream leserStream = new FileStream(@"c:\Uebung\deKatzen.txt", FileMode.Open);
+            StreamReader leser = new StreamReader(leserStream);
+            WriteLine(leser.ReadToEnd());
+            leser.Close();
+
+            FileStream schreibenStream = new FileStream(@"c:\Uebung\deKatzen.txt", FileMode.Create);
+            StreamWriter schreiber = new StreamWriter(schreibenStream);
+            schreiber.WriteLine("hi na");
+            schreiber.Close();
+
+            WriteLine("Welche Katzen möchten Sie begutachten?");
+            int katze = Convert.ToInt32(ReadLine());
+            WriteLine("Katze Nummer {0}", katze);
+
+            ReadLine();
+        }
+
+        void anlegen(int id, int bj, double stand, int leist)
+        {
+            id = katzenID;
+            bj = baujahr;
+            stand = futterStand;
+            leist = schlafLeistung;
+        }
+
+    }
+}
