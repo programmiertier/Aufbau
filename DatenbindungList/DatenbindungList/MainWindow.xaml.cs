a14486a68ecf8b0df3a76131a8eec59a47ef8992
programmiertier <zimmermannveronique@protonmail.com>
2018-01-25 09:41:07 +0100

    Bindings und Buttons

---
+++ DatenbindungList/DatenbindungList/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Datenbindung01;
+using System.Collections.ObjectModel;
+
+namespace DatenbindungList
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> liste = new ObservableCollection<Person>();
+        public MainWindow()
+        {
+            liste.Add(new Person { name = "V Zim", adresse = "Köln", alter = 31 });
+            liste.Add(new Person { name = "Klaus Kleber", adresse = "Köln", alter = 79 });
+            InitializeComponent();
+            listBox1.ItemsSource = liste;
+            listBox1.DisplayMemberPath = "name";
+            listBox1.SelectedIndex = 0;
+            listBox1.Focus();
+            
+        }
+
+
+        private void btn_bearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_hinzufuegen_Click(object sender, RoutedEventArgs e)
+        {
+            // Binding hinzufügen
+            BindingOperations.ClearBinding(txtName, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtAdresse, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtAlter, TextBox.TextProperty);
+            txtName.Text = txtAlter.Text = txtAdresse.Text = "";
+            txtName.Focus();
+            btn_hinzufuegen.IsEnabled = false;
+            btn_abbrechen.IsEnabled = true;
+            btn_speichern.IsEnabled = true;
+            btn_loeschen.IsEnabled = false;
+        }
+
+        private void btn_speichern_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person();
+            person.name = txtName.Text;
+            person.adresse = txtAdresse.Text;
+            person.alter = Convert.ToInt32(txtAlter.Text);
+            liste.Add(person);
+            btn_hinzufuegen.IsEnabled = false;
+            btn_abbrechen.IsEnabled = true;
+            btn_speichern.IsEnabled = true;
+            btn_loeschen.IsEnabled = true;
+            setBinding();
+            listBox1.SelectedItem = person; // noch einfacher, als die Zeile drunter
+            // listBox1.SelectedIndex = listBox1.Items.Count - 1;  // letztes hinzugefügtes
+        }
+
+        private void btn_abbrechen_Click(object sender, RoutedEventArgs e)
+        {
+            btn_hinzufuegen.IsEnabled = true;
+            btn_loeschen.IsEnabled = true;
+            btn_abbrechen.IsEnabled = false;
+            btn_speichern.IsEnabled = false;
+            listBox1.SelectedIndex = 0;
+            setBinding();
+        }
+
+        private void btn_loeschen_Click(object sender, RoutedEventArgs e)
+        {
+            liste.RemoveAt(Convert.ToInt32(listBox1.SelectedItem));
+        }
+
+        private void setBinding()
+        {
+            // bindung neu festlegen
+            Binding binding = new Binding("name");
+            txtName.SetBinding(TextBox.TextProperty, binding);
+            binding = new Binding("adresse");
+            txtAdresse.SetBinding(TextBox.TextProperty, binding);
+            binding = new Binding("alter");
+            txtAlter.SetBinding(TextBox.TextProperty, binding);
+        }
+
+        private void btnObjekt_Click(object sender, RoutedEventArgs e)
+        {
+            liste[1].adresse = "zuhause";
+            liste[1].alter = 44;
+            liste[1].name = "voll wichtig";
+
+            // geht so auch
+            liste.Add(new Person { name = "Anna Berta", alter = 30, adresse = "Berlin" });
+        }
+    }
+}
---
+++ DatenbindungList/DatenbindungList/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datenbindung01
+{
+    class Person : INotifyPropertyChanged
+    {
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Name"));
+            }
+        }
+        private int? _alter;
+        public int? alter
+        {
+            get { return _alter; }
+            set
+            {
+                _alter = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Alter"));
+            }
+        }
+        private string _adresse;
+        public string adresse
+        {
+            get { return _adresse; }
+            set
+            {
+                _adresse = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Adresse"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
